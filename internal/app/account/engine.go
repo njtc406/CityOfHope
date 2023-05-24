ce0dc24fa0bc1195b286a0ef4f802517050bc218
杨锐 <315483585@qq.com>
2023-05-24 18:07:52 +0800

    基础提交

--- internal/app/account/engine.go
+++ internal/app/account/engine.go
@@ -1,6 +1,6 @@
 // Package account
-// 模块名:
-// 模块功能简介:
+// 模块名: 账号服务
+// 模块功能简介: 用于提供账号相关服务
 package account
 
 import (
@@ -10,10 +10,25 @@ import (
 
 // service 账号服务
 type service struct {
+	// TODO: 账号服务只需要监听rpc消息,然后提供创建和校验接口
+	// TODO: 然后做一个查询服务,也使用rpc,供后台查询使用
+	// TODO: 数据库直接使用mysql来做
+}
+
+func (s *service) Init() {
+
+}
+
+func (s *service) Start() {
+
+}
+
+func (s *service) Stop() {
+
 }
 
 func NewService() engine.IEngine {
-	return nil
+	return &service{}
 }
 
 func init() {
